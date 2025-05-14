13fcb97907fe274b130d1bac1c60af44404cd67c
kvkarandashev <kvkarandashev@gmail.com>
2025-05-14 13:22:24 +0200

    better quickview

--- competent_formatting/bin/table_view.py
+++ competent_formatting/bin/table_view.py
@@ -5,7 +5,7 @@ import subprocess
 from ..str_formatting import replace_special_LaTeX_symbols
 
 
-def wrap_table_into_test_tex(table_source, pdf_title, newcommands=""):
+def wrap_table_into_test_tex(table_source_file, pdf_title, newcommands=""):
     test_source = (
         """
 \\documentclass[journal=jctc,manuscript=article,layout=traditional]{achemso}
@@ -24,9 +24,9 @@ def wrap_table_into_test_tex(table_source, pdf_title, newcommands=""):
 \\maketitle
 
 \\begin{table}
-"""
-        + table_source
-        + """
+\\input{"""
+        + table_source_file[:-4]
+        + """}
 \\end{table}
 \\end{document}
 
@@ -39,9 +39,13 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("filename", type=str)
     parser.add_argument("--replacements", type=str)
-
     args = parser.parse_args()
     filename = args.filename
+
+    if not (len(filename) > 4 and (filename[:-4] != ".tex")):
+        print("Tex filename must end with '.tex'!")
+        quit()
+
     newcommands = ""
     if args.replacements is not None:
         for replacement in args.replacements.split(","):
@@ -49,8 +53,7 @@ def main():
             newcommands += "\\newcommand{" + r1 + "}{" + r2 + "}\n"
 
     pdf_title = "Displaying table: " + replace_special_LaTeX_symbols(filename)
-    table_source = open(filename, "r").read()
-    test_source = wrap_table_into_test_tex(table_source, pdf_title, newcommands=newcommands)
+    test_source = wrap_table_into_test_tex(filename, pdf_title, newcommands=newcommands)
     test_source_filename = "test_table_" + os.path.basename(filename)
     with open(test_source_filename, "w") as f:
         f.write(test_source)
